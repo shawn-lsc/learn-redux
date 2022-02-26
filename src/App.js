ca8ee6f94f9d860d6c161288312afc202919b5fb
shawn-lsc <shawn_lsc@outlook.com>
2022-02-27 01:13:28 +0800

    feat: add App code & style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.scss";
+
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Header></Header>
+        <div className="container">
+        
+        </div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:imdbID" component={MovieDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
